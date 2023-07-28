2eca14e1abb591f85a8f674d32fc335f08dc933b
lagartaSadia <campolino.l@gmail.com>
2023-07-28 01:54:01 -0300

    Giving form to the app, need to change first text for image

--- app/src/main/java/com/example/album/MainActivity.kt
+++ app/src/main/java/com/example/album/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +36,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun AlbumApp(
     modifier: Modifier = Modifier
@@ -48,10 +51,31 @@ fun GalleryWithImageAndTextAndButtons() {
     Column(
         modifier = Modifier
             .padding(start = 20.dp, end = 20.dp)
+            .fillMaxWidth(1f)
     ) {
-        Text(text = "Image 1")
-        Text(text = "Maybe a cat, by Lagarta")
-
+        Text(
+            text = "Image 1",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(1f)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Maybe a cat")
+            Text(text = "by Lagarta")
+        }
+        Spacer(modifier = Modifier.size(5.dp))
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun AlbumAppPreview() {
+    AlbumTheme {
+        AlbumApp()
+    }
+}
