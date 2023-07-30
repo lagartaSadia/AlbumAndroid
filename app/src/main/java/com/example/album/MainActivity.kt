9400beace98b87d11bb1be693fc58ba0467e3c35
lagartaSadia <campolino.l@gmail.com>
2023-07-30 20:09:32 -0300

    Add Image and Buttons

--- app/src/main/java/com/example/album/MainActivity.kt
+++ app/src/main/java/com/example/album/MainActivity.kt
@@ -3,18 +3,23 @@ package com.example.album
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.album.ui.theme.AlbumTheme
@@ -37,10 +42,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AlbumApp(
-    modifier: Modifier = Modifier
-        .fillMaxSize()
-) {
+fun AlbumApp() {
     AlbumTheme {
         GalleryWithImageAndTextAndButtons()
     }
@@ -50,15 +52,12 @@ fun AlbumApp(
 fun GalleryWithImageAndTextAndButtons() {
     Column(
         modifier = Modifier
-            .padding(start = 20.dp, end = 20.dp)
-            .fillMaxWidth(1f)
+            .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 20.dp)
+            .fillMaxWidth(1f),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Image 1",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth(1f)
-        )
+        Image(painter = painterResource(R.drawable.teiu_1), contentDescription = "Teiú")
         Spacer(modifier = Modifier.size(10.dp))
         Column(
             modifier = Modifier
@@ -69,6 +68,15 @@ fun GalleryWithImageAndTextAndButtons() {
             Text(text = "by Lagarta")
         }
         Spacer(modifier = Modifier.size(5.dp))
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Voltar")
+            }
+            Spacer(modifier = Modifier.size(5.dp))
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Avançar")
+            }
+        }
     }
 }
 
