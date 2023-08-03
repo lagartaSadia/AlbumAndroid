32c1eeb49896ffcf692c717e8cc97934f1d7b03c
lagartaSadia <campolino.l@gmail.com>
2023-08-03 16:33:23 -0300

    Iteration with app completed

--- app/src/main/java/com/example/album/MainActivity.kt
+++ app/src/main/java/com/example/album/MainActivity.kt
@@ -17,9 +17,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.album.ui.theme.AlbumTheme
@@ -50,30 +56,75 @@ fun AlbumApp() {
 
 @Composable
 fun GalleryWithImageAndTextAndButtons() {
+    var returnButton by remember {
+        mutableStateOf(false)
+    }
+    var fowardButton by remember {
+        mutableStateOf(true)
+    }
+    var imageNumber by remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when(imageNumber) {
+        1 -> R.drawable.teiu_1
+        2 -> R.drawable.teiu_2
+        3 -> R.drawable.teiu_3
+        else -> R.drawable.teiu_4
+    }
+    val imageAuthor = when(imageNumber) {
+        1 -> R.string.description_1
+        2 -> R.string.description_2
+        3 -> R.string.description_3
+        else -> R.string.description_4
+    }
+
     Column(
         modifier = Modifier
             .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 20.dp)
-            .fillMaxWidth(1f),
+            .fillMaxSize(1f),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(painter = painterResource(R.drawable.teiu_1), contentDescription = "Teiú")
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(1f),
+            painter = painterResource(imageResource),
+            contentDescription = "Teiú",
+            contentScale = ContentScale.FillWidth)
         Spacer(modifier = Modifier.size(10.dp))
         Column(
             modifier = Modifier
                 .fillMaxWidth(1f),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Maybe a cat")
-            Text(text = "by Lagarta")
+            Text(text = stringResource(imageAuthor))
+            Text(text = stringResource(R.string.author_name))
         }
-        Spacer(modifier = Modifier.size(5.dp))
-        Row {
-            Button(onClick = { /*TODO*/ }) {
+        Row(
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Button(enabled = returnButton, onClick = {
+                imageNumber -= 1
+                if (imageNumber == 1) {
+                    returnButton = false
+                }
+                if (imageNumber != 4) {
+                    fowardButton = true
+                }
+            }) {
                 Text(text = "Voltar")
             }
             Spacer(modifier = Modifier.size(5.dp))
-            Button(onClick = { /*TODO*/ }) {
+            Button(enabled = fowardButton, onClick = {
+                imageNumber += 1
+                if (imageNumber != 1) {
+                    returnButton = true
+                }
+                if (imageNumber == 4) {
+                    fowardButton = false
+                }
+            }
+            ) {
                 Text(text = "Avançar")
             }
         }
